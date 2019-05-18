80a1da83d729097865e87a23e342122fa4ea57cc
Santosh Reddy Rekula <santosh@Santoshs-MacBook-Pro.local>
2019-05-19 01:56:03 +1000

    init

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ class HelloWorld {
     // Prints "Hello, World" to the terminal window.
     public static void main(String args[]) {
         System.out.println("Hello, World - git repo");
+        System.out.println("Poll SCM - check");
     }
 }
